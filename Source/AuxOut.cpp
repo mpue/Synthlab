7418559366a2b2dfb46a13dc3c949635bd9d0dfe
Matthias Pueski <matthias@pueski.de>
2018-04-23 16:19:16 +0200

    Mixer

---
+++ Source/AuxOut.cpp
@@ -0,0 +1,9 @@
+//
+//  AuxOut.cpp
+//  Synthlab - App
+//
+//  Created by Matthias Pueski on 23.04.18.
+//  Copyright © 2018 Pueski. All rights reserved.
+//
+
+#include "AuxOut.hpp"
---
+++ Source/AuxOut.h
@@ -0,0 +1,14 @@
+//
+//  AuxOut.hpp
+//  Synthlab - App
+//
+//  Created by Matthias Pueski on 23.04.18.
+//  Copyright © 2018 Pueski. All rights reserved.
+//
+
+#ifndef AuxOut_hpp
+#define AuxOut_hpp
+
+#include <stdio.h>
+
+#endif /* AuxOut_hpp */
