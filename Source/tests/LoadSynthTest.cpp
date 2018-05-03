e0969881fa87d673f6276f38b0c9fb23f7ab206a
InSL <n.schwenkel@me.com>
2018-05-03 22:44:03 +0200

    clean up

--- Source/tests/LoadSynthTest.cpp
+++ Source/tests/LoadSynthTest.cpp
@@ -8,4 +8,55 @@
   ==============================================================================
 */
 
-#include "LoadSynthTest.h"
+/*
+ ==============================================================================
+ 
+ LoadSynthTest.h
+ Created: 26 Apr 2018 10:19:59am
+ Author:  Matthias Pueski
+ 
+ ==============================================================================
+ */
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "../Project.h"
+#include "../AudioManager.h"
+#include "../Plugins/PluginManager.h"
+#include "../MainComponent.h"
+
+
+class LoadSynthTest : public UnitTest {
+    
+public:
+    LoadSynthTest() : UnitTest("LoadSynthTest") {
+        
+    }
+    
+    void runTest() override {
+        beginTest("LoadSynth");
+        MainComponent* mc = new MainComponent();
+        expect(AudioManager::getInstance()->getDeviceManager() != nullptr);
+        
+        expect(mc->getEditor() != nullptr);
+        expect(mc->getEditor()->getMixer() != nullptr);
+        
+        File f = File(File::getCurrentWorkingDirectory().getFullPathName()+"/../../../../Regression/NoiseTest.xml");
+        expect(f.exists());
+        Logger::writeToLog(f.getFullPathName());
+        
+        String data = f.loadFileAsString();
+        
+        mc->getEditor()->loadFromString(data);
+        
+        expect(mc->getEditor()->getModule() != nullptr);
+        expect(mc->getEditor()->getModule()->getConnections()->size() == 3);
+        expect(mc->getEditor()->getModule()->getModules()->size() == 3);
+        
+        delete mc;
+        JUCEApplication::getInstance()->quit();
+    }
+    
+};
+
+static LoadSynthTest loadSynthTest;
+
