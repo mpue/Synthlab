de576cff6c625f8b36d679f30bb84a4a42e5f2cd
InSL <n.schwenkel@me.com>
2018-05-03 22:45:35 +0200

    add ModuleTest

---
+++ Source/tests/ModuleTest.cpp
@@ -0,0 +1,46 @@
+/*
+  ==============================================================================
+
+    ModuleTest.cpp
+    Created: 3 May 2018 9:46:08pm
+    Author:  Nils Schwenkel
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "../Project.h"
+#include "../AudioManager.h"
+#include "../Plugins/PluginManager.h"
+#include "../MainComponent.h"
+
+
+class ModuleTest : public UnitTest {
+    
+public:
+    ModuleTest() : UnitTest("ModuleTest") {
+        
+    }
+    
+    void runTest() override {
+        
+        
+        beginTest("asdf");
+        Module* pModule = new Module();
+        std::vector<Pin*>& pins = pModule->getPins();
+        
+        expectEquals<size_t>(0u, pins.size());
+    }
+    
+    void newModule()
+    {
+        beginTest("newModule");
+        Module* pModule = new Module();
+        
+        expect(nullptr != pModule);
+        delete pModule;
+    }
+    
+};
+
+static ModuleTest moduleTest;
