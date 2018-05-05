cc5cc1122e69ccdc2c02626ce43b466c66ca20f2
Matthias Pueski <matthias@pueski.de>
2018-05-05 15:36:01 +0200

    SynthEditorTest

---
+++ Source/tests/SynthEditorTest.cpp
@@ -0,0 +1,61 @@
+/*
+  ==============================================================================
+
+    LoadSynthTest.cpp
+    Created: 26 Apr 2018 10:22:50am
+    Author:  Matthias Pueski
+
+  ==============================================================================
+*/
+
+/*
+ ==============================================================================
+ 
+ LoadSynthTest.h
+ Created: 26 Apr 2018 10:19:59am
+ Author:  Matthias Pueski
+ 
+ ==============================================================================
+ */
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "../Project.h"
+#include "../AudioManager.h"
+#include "../Plugins/PluginManager.h"
+#include "../MainComponent.h"
+
+using juce::File;
+using juce::UnitTest;
+using juce::Logger;
+using juce::String;
+using juce::JUCEApplication;
+
+#include <gtest.h>
+
+TEST(SynthEditorTest, load)
+{
+    MainComponent* mc = new MainComponent();
+
+    ASSERT_NE(nullptr, AudioManager::getInstance()->getDeviceManager());
+    ASSERT_NE(nullptr, mc->getEditor());
+    ASSERT_NE(nullptr, mc->getEditor()->getMixer());
+    File f = File(File::getCurrentWorkingDirectory().getFullPathName()+"/../../../../Regression/NoiseTest.xml");
+    ASSERT_EQ(true, f.exists());
+    
+    Logger::writeToLog(f.getFullPathName());
+    
+    String data = f.loadFileAsString();
+    
+    mc->getEditor()->loadFromString(data);
+    
+    ASSERT_NE(nullptr,mc->getEditor()->getModule());
+    ASSERT_EQ(3,mc->getEditor()->getModule()->getConnections()->size());
+    ASSERT_EQ(3,mc->getEditor()->getModule()->getModules()->size());
+    
+    delete mc;
+    // JUCEApplication::getInstance()->shutdown();
+    JUCEApplication::getInstance()->quit();
+}
+
+
+
